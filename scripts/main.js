e6a921b66fb5fd7b0482546189f69d61f5927727
Josiah Allen <josiah_g_allen@yahoo.com>
2015-09-18 16:39:18 -0500

    more styleing, login works

--- scripts/main.js
+++ scripts/main.js
@@ -6,14 +6,58 @@ $(document).ready(function() {
     var $form = $('form');
     var $picWindow = $('#pic-window');
     var $cancel = $('a');
+    var $registerBtn = $('#register-button');
+    var $newUemail = $('#newuemail');
+    var $createPass = $('#createpass');
+    var $confirmPass = $('#confirmpass')
 
     var url = 'http://tiyfe.herokuapp.com/collections/josiah-myPix';
+    var urlUsers = 'http://tiyfe.herokuapp.com/collections/josiah-myPix-users';
+
+    $registerBtn.click(function(e) {
+        $('#login').hide();
+        $registerBtn.hide();
+        $('#sign-up').show();
+        $('#back').show();
+    });
+
+    $('#back').click(function(e){
+        $('#login').show();
+        $('#back').hide();
+        $('#sign-up').hide();
+        $registerBtn.show();
+    })
+
+    $('#login').submit(function(e){
+        e.preventDefault();
+        var loginUser = $('#newuser').val();
+        var password = $('#log-pass').val();
+        console.log(loginUser);
+        console.log(password);
+        $.get(
+            urlUsers,
+            function(response) {
+                response.forEach(function(response){
+                    if (loginUser === response.username && password === response.password) {
+                        $('#loginboxwrapper').toggle('up');
+                        $('#sitewrapper').toggle('down');
+                        $('#error').css({color: 'white'});
+                    }
+                })
+            },
+            'json'
+            )
+    })
+
+    // $('#sign-up').submit(function(){
+    //     var 
+    // })
 
     $.get(
         url,
         function(response) {
             response.forEach(function(response) {
-                $picWindow.prepend('<div class="post"><img src="' + response.picture + '"><div>' + response.caption + '</div></div>');
+                $picWindow.append('<div class="post"><img src="' + response.picture + '"><div class="cap">' + response.caption + '</div></div>');
             });
         },
         'json'
@@ -24,7 +68,7 @@ $(document).ready(function() {
     })
 
     $form.submit(function(e) {
-        e.preventDefault;
+        e.preventDefault();
         var picURL = $('#new-pix').val();
         var caption = $('#new-pixcap').val();
         var $error = $('#error')
@@ -35,23 +79,28 @@ $(document).ready(function() {
                 caption: caption
             },
             function(response) {
-                $picWindow.append('<div class="post"><img src="' + response.picture + '"><div>' + response.caption + '</div></div>');
+                $picWindow.append('<div class="post"><img src="' + response.picture + '"><div class="cap">' + response.caption + '</div></div>');
             },
             'json'
         )
         $('#new-pix').val('');
     	$('#new-pixcap').val('');
-    	$error.hide();
+        $error.css({color: 'white'});
     } else {
-    	$error.show();
+    	$error.css({color: 'red'});
     }
     })
 
 
-    $cancel.click(function(){
-    	$('#error').hide();
+    $cancel.click(function(e){
+        e.preventDefault();
+        $('#newuser').val('');
+        $('#log-pass').val('');
+        $newUemail.val('');
+        $createPass.val('');
+        $confirmPass.val('');
+    	$('#error').css({color: 'white'});
 		$('#new-pix').val('');
 		$('#new-pixcap').val('');
-
     })
 })
